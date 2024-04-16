89e33cb7a4b46531be2633d6f5f2b893adcfa0e1
bhnur <bhnur@kth.se>
2024-04-16 18:28:57 +0200

    Created testMotor.ino has functions for Starting a motor at a set speed and stopping the motor.

---
+++ feeder/testMotor/testMotor.ino
@@ -0,0 +1,40 @@
+// Motor connection pins
+int enA = 9; // Enable pin
+int in1 = 8; // Input 1
+int in2 = 7; // Input 2
+
+void setup() {
+  Serial.begin(9600);
+  // set motor control pins to out
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+
+// Turn off motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+}
+
+void loop() {
+
+}
+
+void startMotor(int speed) {
+  digitalWrite(in1, HIGH);
+	digitalWrite(in2, LOW);
+  speed(speed);
+} 
+
+void motorOff() {
+  digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+}
+
+
+void speed(int speed){
+  if (speed >=256){
+    Serial.write("too much speed");
+    return;
+  }
+  analogWrite(enA,speed);
+}
