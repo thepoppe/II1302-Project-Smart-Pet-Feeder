4f0505c274d004bf37d32c0f8287886eb35f6649
bhnur <bhnur@kth.se>
2024-04-17 10:39:03 +0200

    Created libraries folder to store custom made libraries. Created Motor library, including functions to initate (set outputs) start/stop motor and set speed.

--- feeder/info.md
+++ feeder/info.md
@@ -1,1 +1,3 @@
 folder for arduino code
+
+- libraries. Includes zip files which you can import to your own sketch. Go to Sketch > Include Library > add .ZIP library then select the library you want.
--- feeder/testMotor/testMotor.ino
+++ feeder/testMotor/testMotor.ino
@@ -1,45 +1,20 @@
-// Motor connection pins
-int enA = 9; // Enable pin
-int in1 = 8; // Input 1
-int in2 = 7; // Input 2
+#include <Motor.h>
 
+int en = 9;
+int in1 = 8;
+int in2 = 7;
+Motor Motor(en,in1,in2);
 void setup() {
-  Serial.begin(9600);
-  // set motor control pins to out
-  pinMode(enA, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-
-// Turn off motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
+  // put your setup code here, to run once:
+ Motor.initMotor();
 }
 
 void loop() {
-  startMotor(100);
+  Motor.startMotor();
   delay(2000);
-  speed(50);
+  Motor.speed(255);
   delay(2000);
-  stopMotor();
+  Motor.speed(20);
   delay(2000);
-}
-
-void startMotor(int speed) {
-  digitalWrite(in1, HIGH);
-	digitalWrite(in2, LOW);
-  speed(speed);
-} 
-
-void stopMotor() {
-  digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
-}
-
-
-void speed(int speed){
-  if (speed >=256){
-    Serial.write("too much speed");
-    return;
-  }
-  analogWrite(enA,speed);
+  Motor.stopMotor();
 }
