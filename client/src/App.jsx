5446366194a8794378a6ce7f06f0baf350eb6d90
lukasja <lukasja@kth.se>
2024-04-18 16:04:07 +0200

    current time displayed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./index.css";
+
+
+
 
 function App() {
   const [content, setContent] = useState("none");
-  const [msg, setMsg] = useState("default");
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
 
-  
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+
+    }, 1000);
+  }, []);
+
   function startMotor() {
     fetch(`http://localhost:3000/start-motor`)
       .then((response) => response.text())
-      .then((data) => setContent(data) )
+      .then((data) => setContent(data))
       .catch((error) => console.error("Error:", error));
   }
-
-
 
   return (
     <div className="centered-button">
       <p>{content}</p>
-      <button onClick={() => startMotor()}>
+      <p>Current Time: {time}</p>
+      <button onClick={startMotor}>
         Start Motor
       </button>
     </div>
   );
-
 }
+
 export default App;
