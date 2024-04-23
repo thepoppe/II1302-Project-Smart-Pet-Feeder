15c6c9b1b1ae05d57c49d31a2454fb570cfacd34
lukasja <lukasja@kth.se>
2024-04-23 17:39:28 +0200

    new endpoints in express server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 const defaultMsg = "HelloWorld";
 let motorStatus=false
+let schedules = [];
+let distanceSensorValue=null
 
 app.use(cors());
 app.use(express.json());
@@ -11,7 +13,6 @@ app.use(express.json());
 // Toggle motor status ( done by our application when pressing a button) 
 app.post('/toggle-motor', (req, res) => {
   motorStatus = !motorStatus; //TODO make toggle motorstatus function in model
-
   if(motorStatus)
     res.send("Motor is running");
   else
@@ -23,6 +24,29 @@ app.post('/toggle-motor', (req, res) => {
 app.get('/motor-status', (req, res) => {
   res.send(motorStatus)
 });
+
+// add a new schedule to the schedule array
+app.post('/schedule', (req, res) => {
+  const {day, hour, minute } = req.body;
+  const completed=false
+  schedules.push({day, hour, minute,completed});
+  console.log(schedules) 
+  res.json({ message: "Schedule added " });
+});
+
+// Endpoint to get all schedules in the schedules array
+app.get('/getschedules', (req, res) => {
+  res.json(schedules); 
+});
+
+// Endpoint to update the current value of the distance sensor
+app.post('/uploadDistanceSensorValue', (req, res) => {
+  const {value} = req.body;
+  distanceSensorValue=value
+  console.log(distanceSensorValue) 
+  res.json({currentValue: distanceSensorValue });
+});
+
 
 app.get("/", (req, res) => {
   res.send(message);
