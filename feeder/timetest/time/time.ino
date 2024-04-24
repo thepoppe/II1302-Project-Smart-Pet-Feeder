3f274dd729520294d84b716c5991d270929ddf02
bhnur <bhnur@kth.se>
2024-04-24 15:03:57 +0200

    combined both shcedule and time test so we can check if current time = shceduled

--- feeder/timetest/time/time.ino
+++ feeder/timetest/time/time.ino
@@ -15,7 +15,7 @@ const char* ssid = "iPhone123";
 const char* password = "12345678";
 
 const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 0;
+const long  gmtOffset_sec = 3600;
 const int   daylightOffset_sec = 3600;
 
 void setup(){
@@ -52,30 +52,21 @@ void printLocalTime(){
     Serial.println("Failed to obtain time");
     return;
   }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-  Serial.print("Day of week: ");
-  Serial.println(&timeinfo, "%A");
-  Serial.print("Month: ");
-  Serial.println(&timeinfo, "%B");
-  Serial.print("Day of Month: ");
-  Serial.println(&timeinfo, "%d");
-  Serial.print("Year: ");
-  Serial.println(&timeinfo, "%Y");
-  Serial.print("Hour: ");
-  Serial.println(&timeinfo, "%H");
-  Serial.print("Hour (12 hour format): ");
-  Serial.println(&timeinfo, "%I");
-  Serial.print("Minute: ");
-  Serial.println(&timeinfo, "%M");
-  Serial.print("Second: ");
-  Serial.println(&timeinfo, "%S");
+  
 
   Serial.println("Time variables");
   char timeHour[3];
   strftime(timeHour,3, "%H", &timeinfo);
+  Serial.print("hour:" );
   Serial.println(timeHour);
+  char timeMinute[3];
+  strftime(timeMinute,3, "%M", &timeinfo);
+  Serial.print("Minutes:" );
+  Serial.println(timeMinute);
   char timeWeekDay[10];
   strftime(timeWeekDay,10, "%A", &timeinfo);
   Serial.println(timeWeekDay);
   Serial.println();
+
+
 }
