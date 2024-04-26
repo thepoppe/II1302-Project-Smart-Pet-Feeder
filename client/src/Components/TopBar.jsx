896528a1ed00ba329c4a52eb6ff2a3d16da671e3
thePoppe <pontus.morin@hotmail.se>
2024-04-26 13:50:10 +0200

    removed temporary files

--- client/src/Components/TopBar.jsx
+++ client/src/Components/TopBar.jsx
@@ -12,16 +12,7 @@ export default function TopBar(props) {
             🐾 Smart Feeder
           </Link>
         </li>
-        {loggedIn && (
-        <div className="dropdown">
-          <Link to="/settings">
-            <h2>Profile</h2>
-          </Link>
-          <div className="dropdown-content">
-            <button className="dropdownbtn">Profile</button>
-            <button onClick={() => props.logOut()} className="dropdownbtn">Log Out</button>
-          </div>
-        </div>
+          
       </ul>
     </nav>
     </>
