e42d31f3a8cc05ec43b418c409eecb3b104ce2f4
thepoppe <pontus.morin@hotmail.se>
2024-04-28 19:20:56 +0200

    dropdown readded

--- client/src/Components/TopBar.jsx
+++ client/src/Components/TopBar.jsx
@@ -3,20 +3,29 @@ import "./TopBar.css";
 export default function TopBar(props) {
   let loggedIn = props.loggedIn;
   return (
-
     <>
-      <nav className="navigation">
-      <ul>
-        <li>
-          <Link to="/" className="brand-logo">
-            🐾 Smart Feeder
-          </Link>
-        </li>
-          
-      </ul>
-    </nav>
+      <nav>
+        <ul className="navigation">
+          <li className="navcenter-link">
+            <Link to="/" className="brand-logo">
+              <h2>🐾 Smart Feeder</h2>
+            </Link>
+          </li>
+          {loggedIn && (
+            <div className="dropdown">
+              <Link to="/settings">
+                <p>Profile</p>
+              </Link>
+              <div className="dropdown-content">
+                <button className="dropdownbtn">About</button>
+                <button onClick={() => props.logOut()} className="dropdownbtn">
+                  Log Out
+                </button>
+              </div>
+            </div>
+          )}
+        </ul>
+      </nav>
     </>
-
   );
 }
-
