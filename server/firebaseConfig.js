07ba48e8819e26387d018708bdb95a826ecacc10
fAribAmo <94m.fariba@gmail.com>
2024-05-01 19:22:19 +0200

    firebaseConfig

---
+++ server/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDHC-2RucczYbdYR7KWkISiwRLxcI8lMAA",
+    authDomain: "smart-pet-feeder-c7fd2.firebaseapp.com",
+    projectId: "smart-pet-feeder-c7fd2",
+    storageBucket: "smart-pet-feeder-c7fd2.appspot.com",
+    messagingSenderId: "846041009854",
+    appId: "1:846041009854:web:24cf37ecdd05b8f31498ef",
+    measurementId: "G-SF8QL33S80"
+  };
