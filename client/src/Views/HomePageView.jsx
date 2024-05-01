9f13f533aaa2532062e42147a05c88d4d6ad91b9
thepoppe <pontus.morin@hotmail.se>
2024-05-01 12:36:44 +0200

    moved the test elements to its own page to avoid clutter

--- client/src/Views/HomePageView.jsx
+++ client/src/Views/HomePageView.jsx
@@ -76,41 +76,6 @@ function HomePageView(props) {
           </Link>
         </ul>
       </div>
-
-      <button
-        onClick={() => {
-          const requestBody = {
-            key1: "dog1",
-            key2: "cat1",
-          };
-
-          fetch("http://localhost:3000/testSetDB", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(requestBody),
-          })
-            .then((response) => response.text())
-            .then((text) => console.log(text))
-            .catch((error) => console.error("Error:", error));
-        }}
-      >
-        Test SET database
-      </button>
-
-      <button
-        onClick={() => {
-          fetch("http://localhost:3000/testGetDB", {
-            method: "GET",
-          })
-            .then((response) => response.text())
-            .then((text) => console.log(text))
-            .catch((error) => console.error("Error:", error));
-        }}
-      >
-        Test GET database
-      </button>
     </>
   );
 }
