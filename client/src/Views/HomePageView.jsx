4782bec63cffac066c7b81181ff883bf5f956adc
thepoppe <pontus.morin@hotmail.se>
2024-05-08 21:35:48 +0200

    removed homepage to view deplyment

--- client/src/Views/HomePageView.jsx
+++ client/src/Views/HomePageView.jsx
@@ -1,27 +1,38 @@
 import { Link } from "react-router-dom";
 
-import {Canvas} from "@react-three/fiber";
-import {useGLTF, Stage, PresentationControls, OrbitControls, PerspectiveCamera} from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import {
+  useGLTF,
+  Stage,
+  PresentationControls,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
 import { Suspense } from "react";
-import "./homePage.css"
+import "./homePage.css";
+
+/*
 function Model(props){
   const {scene} = useGLTF("../../feeder.glb");
   return <primitive  object={scene} {...props}/>
 }
+*/
 
 function HomePageView(props) {
   return (
     <>
-    <div
-  style={{
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "50vh", // This will make the parent div take the full height of the viewport
-    width: "100vw", // This will make the parent div take the full width of the viewport
-    
-  }}
->
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "50vh", // This will make the parent div take the full height of the viewport
+          width: "100vw", // This will make the parent div take the full width of the viewport
+        }}
+      >
+        <p>Picture here CAD removed to deploy</p>
+
+        {/*
       <div
         className="threeDstyle"
         style={{
@@ -34,7 +45,7 @@ function HomePageView(props) {
         }}>
           <Suspense>
         <Canvas shadows className="threeDstyle" dpr={[1,2]}    style={{
-                  width: '100%', // Canvas fills the container div
+          width: '100%', // Canvas fills the container div
                   height: '100%', // Canvas fills the container div
                   }} >
                  <ambientLight intensity={0.5} />
@@ -49,22 +60,28 @@ function HomePageView(props) {
         </Suspense>
         
         </div>
+*/}
       </div>
 
       <div>
         <ul className="homeUL">
           <Link to="/schedule">
-            <button className="LISchedule homeBTN" title="View your schedule">
-              
-            </button>
+            <button
+              className="LISchedule homeBTN"
+              title="View your schedule"
+            ></button>
           </Link>
           <Link to="/status">
-            <button className="LIStatus homeBTN" title="View Feeder status">
-            </button>
+            <button
+              className="LIStatus homeBTN"
+              title="View Feeder status"
+            ></button>
           </Link>
           <Link to="/settings">
-            <button className="LISetting homeBTN" title="View your settings">
-            </button>
+            <button
+              className="LISetting homeBTN"
+              title="View your settings"
+            ></button>
           </Link>
         </ul>
       </div>
