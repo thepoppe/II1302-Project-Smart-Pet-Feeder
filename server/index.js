7b848889a03c7936f18adc3a0a32e32728ef3d29
lukasja <lukasja@kth.se>
2024-05-08 16:09:07 +0200

    CLEANUP removed unused endpoints and console.logs

--- server/index.js
+++ server/index.js
@@ -26,9 +26,6 @@ let schedules = [];
 let usedSchedules= []
 let distanceSensorValue=null
 let weightSensorValue=null
-
-
-
 const transporter = nodemailer.createTransport({
  service:'gmail',
   auth:{
@@ -38,26 +35,6 @@ const transporter = nodemailer.createTransport({
   secure: true
 })
 
-/************** end point for testing send email */
-app.post("/sendmail", (req, res)=>{
-  
-  const mailData = {
-  from: 'smart.feeder14@gmail.com',  // sender address
-  to: 'ahmadmatar8@gmail.com',   // list of receivers
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-  }
-
-  console.log("got post req")
-
-  transporter.sendMail(mailData, function(error, info){
-    if (error) {
-      res.json({message : error})
-    } else {
-      res.json({message : 'email send'})
-    }
-  });
-} );
 
 
 app.use(cors());
@@ -70,126 +47,13 @@ app.get("/testGetDB", handleGetUserRequest);
 //auth
 
 app.post("/login", handleAuthRequest);
-
-// Toggle motor status ( done by our application when pressing a button) 
-app.post('/toggle-motor', (req, res) => {
-  motorStatus = !motorStatus; //TODO make toggle motorstatus function in model
-  if(motorStatus)
-    res.send("Motor is running");
-  else
-    res.send("Motor is stopped");
-
-});
-
-// respond with the current value of motorStatus 
-app.get('/motor-status', (req, res) => {
-  res.send(motorStatus)
-});
-
-
-// add a new schedule to the schedule array
-app.post('/schedule', (req, res) => {
-  const {month, day, hour, pet, amount, minute} = req.body;
-  
-  // Validation to check if date is in the past
-  const now = new Date(); 
-  const scheduleDate = new Date(now); 
-  scheduleDate.setMonth(month);
-  scheduleDate.setDate(day);
-  scheduleDate.setHours(hour);
-  scheduleDate.setMinutes(minute);
-  if (scheduleDate < now) {
-    return res.status(400).json({ message: "Cannot add a schedule in the past." });
-  }
-  
-
- // Validation done. Add to schedules
-  schedules.push({month,day, hour, minute});
-  schedules.sort(compareDatesCB);
- 
-  res.json({ success: true, message: "Schedule added " });
-});
-
-// Endpoint to get the all schedules in the schedules array (frontend)
-//TODO Fix pet amount
-app.get('/allSchedules', (req, res) => {
-  const schdles = schedules.map(schedule => ({
-    date: `${(schedule.month + 1).toString().padStart(2, '0')}.${schedule.day.toString().padStart(2, '0')}`,
-    time: `${schedule.hour.toString().padStart(2, '0')}:${schedule.minute.toString().padStart(2, '0')}`,
-    pet: "placeholder pet",
-    amount: "placeholder amount"
-  }));
-  //console.log(schdles)
-  res.json(schdles);
-});
-
-// Endpoint to get the first value in the schedules array ( arduino )
-app.get('/getschedules', (req, res) => {
-  res.json(schedules[0]); 
-});
-
-// test endpoint to remove a completed schedule and add it to completed schedules
-app.get('/removeSchedule', (req, res) => {
-  let first=schedules.shift()
-  usedSchedules.unshift(first)
-  res.json({ message: "Schedule removed" });
-});
-
-
-app.post('/uploadDistanceSensorValue', async (req, res) => {
-  const {dist, weight} = req.body;
-  distanceSensorValue = dist;
-  weightSensorValue = weight;
-  console.log("distance", distanceSensorValue); 
-  console.log("wieght", weightSensorValue);
-  console.log("uid ",uid);
-
-  
- const userEmail = await getUserEmail(uid);
-
-  console.log("email", userEmail)
-  if(distanceSensorValue < 20){
-   
-    const mailData = {
-      from: 'smart.feeder14@gmail.com',  // sender address
-      to: userEmail,   // list of receivers
-      subject: 'Sending Email using Node.js',
-      text: 'low food level fyll now!!!'
-      }
-
-      transporter.sendMail(mailData, function(error, info){
-        if (error) {
-          console.log(error);
-          res.json({message : error})
-        } else {
-          res.json({message : 'email send'})
-        }
-      });
-
-      
-  }
-  res.json({ distance: distanceSensorValue, weight: weightSensorValue });
-});
-
-//Endpoint to get the value of distance sensor
-
-app.get('/sensor-values', (req, res) => {
-  res.json({ distance: distanceSensorValue, weight: weightSensorValue });
-});
-
-
-
-
 app.get("/", (req, res) => {
   res.send(message);
 });
 
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-
 
 // FIRESTORE FUNCTIONS BELOW
 
@@ -199,8 +63,6 @@ app.post('/users/:userId/uploadSensorValues', async (req, res) => {
   const { dist, weight } = req.body;
 
   const userEmail = await getUserEmail(userId);
-
-  console.log("email", userEmail)
   if(dist < 20){
 
     const mailData = {
@@ -221,7 +83,7 @@ app.post('/users/:userId/uploadSensorValues', async (req, res) => {
 
       
   }
-  console.log(userId)
+
   // Validation
   if (dist === undefined || weight === undefined) {
     return res.status(400).json({ error: "Invalid weight or distance data" });
@@ -251,6 +113,17 @@ app.get('/users/:userId/sensorValues', async (req, res) => {
 app.post('/users/:userId/schedules', async (req, res) => {
   const { userId } = req.params;
   const {month, day,hour,minute,amount,pet } = req.body;
+
+   // Validation to check if date is in the past
+   const now = new Date(); 
+   const scheduleDate = new Date(now); 
+   scheduleDate.setMonth(month);
+   scheduleDate.setDate(day);
+   scheduleDate.setHours(hour);
+   scheduleDate.setMinutes(minute);
+   if (scheduleDate < now) {
+     return res.status(400).json({ message: "Cannot add a schedule in the past." });
+   }
   try {
   
     await addSchedule(userId,day,hour,month,minute,pet,amount);
@@ -309,8 +182,7 @@ app.delete('/users/:userId/schedules', async (req, res) => {
 app.post('/users/:userId/removeSchedule', async (req, res) => {
   const { userId } = req.params;
   const {id} = req.body;
-  console.log(userId)
-  console.log(id)
+
   try {
     const result = await removeScheduleWithId(userId, { id });
     if (result) {
@@ -332,7 +204,7 @@ app.get('/users/:userId/next-schedule', async (req, res) => {
   }
   try {
     const nextSchedule = await getNextSchedule(userId);
-    //console.log(nextSchedule)
+    
     res.json(nextSchedule);
   } 
      catch (error) {
@@ -347,10 +219,7 @@ app.get('/users/:userId/next-schedule', async (req, res) => {
 app.post('/users/:userId/pets', async (req, res) => {
   const { userId } = req.params;
   const { petName, petType, petAmount} = req.body;
-  console.log("body: ", req.body);
-  console.log("name", petName)
-  console.log("type", petType)
-  console.log("amount", petAmount)
+
   try {
     await addPet(userId, petName, petType, petAmount);
     res.status(201).send({message : 'Pet added successfully'});
@@ -364,7 +233,7 @@ app.get('/users/:userId/pets', async (req, res) => {
   const { userId } = req.params;
   try {
     const pets = await getPets(userId);
-    console.log(pets);
+
     res.json(pets);
   } catch (error) {
     console.error('Failed to retrieve pets:', error);
@@ -376,9 +245,6 @@ app.get('/users/:userId/pets', async (req, res) => {
 app.delete('/users/:userId/pets', async(req, res) => {
   const {userId} = req.params;
   const {name, type, amount} =  req.body;
-
-  console.log("name", name)
-
 
   const result = await deletePet(userId, {name, type, amount});
   try{
